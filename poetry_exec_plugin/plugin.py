7815341d9e3fb645bd10b75511835f09f2147efd
Keat Tang <keat@propelleraero.com.au>
2023-06-06 08:52:39 +1000

    Tidied up repo

--- poetry_exec_plugin/plugin.py
+++ poetry_exec_plugin/plugin.py
@@ -1,14 +1,12 @@
 import os
 import shlex
 import sys
+from typing import Any, List
 
-from cleo.helpers import argument
 from cleo.application import Application
-
+from cleo.helpers import argument
 from poetry.console.commands.env_command import EnvCommand
 from poetry.plugins.application_plugin import ApplicationPlugin
-
-from typing import Any, List
 
 
 def shlex_join(cmd_list: List[str]) -> str:
@@ -21,12 +19,13 @@ def shlex_join(cmd_list: List[str]) -> str:
 
 
 class ExecCommand(EnvCommand):
-
     name = "exec"
     description = "Execute a predefined command from your pyproject.toml."
 
     arguments = [
-        argument("cmd", "The command to run from your pyproject.toml.", multiple=False),
+        argument(
+            "cmd", "The command to run from your pyproject.toml.", multiple=False
+        ),
         argument(
             "arguments",
             "Additional arguments to append to the command.",
@@ -49,8 +48,8 @@ class ExecCommand(EnvCommand):
 
         if not cmd:
             self.line_error(
-                f"\nUnable to find the command '{cmd_name}'. To configure a command you must "
-                "add it to your pyproject.toml under the path "
+                f"\nUnable to find the command '{cmd_name}'. To configure a command "
+                "you must add it to your pyproject.toml under the path "
                 "[tool.poetry-exec-plugin.commands]. For example:"
                 "\n\n"
                 "[tool.poetry-exec-plugin.commands]\n"
@@ -62,18 +61,19 @@ class ExecCommand(EnvCommand):
         full_cmd = f"{cmd} {shlex_join(self.argument('arguments'))}"
         shell = os.environ.get("SHELL", "/bin/sh")
 
-        # Change directory to the folder that contains the pyproject.toml so that the command runs
-        # from that folder (even if you call poetry exec from a subfolder). This mimics the
-        # behaviour of npm/yarn.
+        # Change directory to the folder that contains the pyproject.toml so that
+        # the command runs from that folder (even if you call poetry exec from a
+        # subfolder). This mimics the behaviour of npm/yarn.
         os.chdir(pyproject_folder_path)
 
         self.line(f"Exec: {full_cmd}\n", style="info")
         result = self.env.execute(*[shell, "-c", full_cmd])
 
-        # NOTE: If running on mac or linux nothing will be executed after the previous line. This
-        # is because poetry uses os.execvpe to run the command which means that the current process
-        # is replaced by the command. If on windows it uses subprocess.run which means the code
-        # after this comment will be executed.
+        # NOTE: If running on mac or linux nothing will be executed after the
+        # previous line. This is because poetry uses os.execvpe to run the command
+        # which means that the current process is replaced by the command. If on
+        # windows it uses subprocess.run which means the code after this comment
+        # will be executed.
 
         self.line("\n✨ Done!")
 
